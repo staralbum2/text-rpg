0e58a90260fa2c268a0fe556c7c98e62a4c672b7
tj <staralbum2@gmail.com>
2024-11-18 15:16:11 +0900

    update user status

--- textrpg/src/textrpg/User.java
+++ textrpg/src/textrpg/User.java
@@ -1,5 +1,21 @@
 package textrpg;
 
-public class User {
-
+abstract public class User {
+	private int power;
+	private int defence;
+	private int hp;
+	private int mp;
+	private int evasion;
+	private int accuracy;
+	private int critical;
+	
+	public User(int power,int defence, int hp, int mp, int evasion,int accuracy,int criyical){
+		this.power=power;
+		this.defence=defence;
+		this.hp=hp;
+		this.mp = mp;
+		this.evasion = evasion;
+		this.accuracy = accuracy;
+		this.critical = criyical;
+	}
 }
