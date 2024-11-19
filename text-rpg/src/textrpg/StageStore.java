bfd767fae6f32f591975fca7fa29b3174ae40056
tj <staralbum2@gmail.com>
2024-11-19 11:53:27 +0900

    pdate stageUpdate

--- text-rpg/src/textrpg/StageStore.java
+++ text-rpg/src/textrpg/StageStore.java
@@ -4,7 +4,17 @@ public class StageStore  extends Stage{
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		try {
+			bw.append("=====[모험가 마트]=====\n[1. 구매하기] [2. 판매하기] [3. 돌아가기]");
+			bw.flush();
+			int sel = Integer.parseInt(br.readLine());
+			
+			if (sel == 3)
+				TextRpg.nextStage = "village";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return false;
 	}
 
--- text-rpg/src/textrpg/StageVillage.java
+++ text-rpg/src/textrpg/StageVillage.java
@@ -4,7 +4,19 @@ public class StageVillage  extends Stage{
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		try {
+			bw.append("=====[모험가 마을]=====\n[1. 길드방문] [2. 교회방문] [3. 마이룸]");
+			bw.flush();
+			int sel = Integer.parseInt(br.readLine());
+			if (sel == 1)
+				TextRpg.nextStage = "Guild";
+			else if (sel == 2)
+				TextRpg.nextStage = "Church";
+			else if (sel == 3)
+				TextRpg.nextStage = "Room";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
