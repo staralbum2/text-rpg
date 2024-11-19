aa781642a50465bbb86ad25534db90ac0ea1c347
tj <staralbum2@gmail.com>
2024-11-19 14:11:10 +0900

    add class

--- text-rpg/src/textrpg/Guild.java
+++ text-rpg/src/textrpg/Guild.java
@@ -1,5 +1,5 @@
 package textrpg;
 
 public class Guild {
-
+  
 }
--- text-rpg/src/textrpg/Player.java
+++ text-rpg/src/textrpg/Player.java
@@ -1,5 +1,10 @@
 package textrpg;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	private int money;
+	ArrayList<Guild> member = new ArrayList<>();
+	ArrayList<Inventori> inventori = new ArrayList<Inventori>();
+	
 }
