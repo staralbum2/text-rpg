c0f49b061775083e5b281d710f878509c531ad43
tj <staralbum2@gmail.com>
2024-11-19 12:48:19 +0900

    update map

--- text-rpg/src/textrpg/StageBattle.java
+++ text-rpg/src/textrpg/StageBattle.java
@@ -4,13 +4,13 @@ public class StageBattle  extends Stage{
 
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
--- text-rpg/src/textrpg/StageStore.java
+++ text-rpg/src/textrpg/StageStore.java
@@ -5,11 +5,12 @@ public class StageStore  extends Stage{
 	@Override
 	public boolean update() {
 		try {
-			bw.append("=====[모험가 마트]=====\n[1. 구매하기] [2. 판매하기] [3. 돌아가기]");
+			bw.append("=====[모험가 마트]=====\n[1. 영웅 박스] 1회 2500g\n[2. 아이템 박스] 1회 1500g\n[3. 돌아가기]");
 			bw.flush();
 			int sel = Integer.parseInt(br.readLine());
-			
-			if (sel == 3)
+			if(sel == 1) {}
+			else if(sel == 2) {}
+			else if (sel == 3)
 				TextRpg.nextStage = "village";
 		} catch (Exception e) {
 			e.printStackTrace();
