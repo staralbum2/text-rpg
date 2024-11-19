be10dcce4140176360d6aa2d7717b362c866e403
unknown <staralbum2@gmail.com>
2024-11-19 18:27:45 +0900

    Update church

--- text-rpg/src/textrpg/stage/StageChurch.java
+++ text-rpg/src/textrpg/stage/StageChurch.java
@@ -3,27 +3,39 @@ package textrpg.stage;
 import textrpg.system.TextRpg;
 
 public class StageChurch extends Stage {
-
+	
+	private void healMember() {
+		
+	}
+	private void resurrectionMember() {
+		
+	}
 	@Override
 	public boolean update() {
-		try {
-			bw.append("=====[교회]=====\n[1. 치유하기]\n[2. 부활하기]\n[3. 돌아가기]");
-			bw.flush();
-			int sel = Integer.parseInt(br.readLine());
-			
-			if (sel == 3)
-				TextRpg.nextStage = "village";
-		} catch (Exception e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				bw.append("=====[교회]=====\n[1. 치유하기]\n[2. 부활하기]\n[3. 돌아가기]");
+				bw.flush();
+				int sel = Integer.parseInt(br.readLine());
+				if (sel == 1) {
+					healMember();
+				} else if (sel == 2) {
+					resurrectionMember();
+				} else if (sel == 3) {
+					TextRpg.nextStage = "village";
+					break;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-
 		return false;
 	}
 
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
